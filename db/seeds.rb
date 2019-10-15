8c4bdc13f55b33f713fc75e11e520190650d0317
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-14 20:16:09 -0700

    successful db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'csv'
+
+WORK_FILE = Rails.root.join('db', 'media_seeds.csv')
+puts "Loading raw Work/Media data from #{WORK_FILE}"
+
+work_failures = []
+CSV.foreach(WORK_FILE, :headers => true) do |row|
+  work = Work.new
+  work.id = row['id']
+  work.category = row['category']
+  work.title = row['title']
+  work.publication_year = row['publication_year']
+  successful = work.save
+  if !successful
+    work_failures << work
+    puts "Failed to save work: #{work.inspect}"
+  else
+    puts "Created work: #{work.inspect}"
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} work(s) failed to save"
 
