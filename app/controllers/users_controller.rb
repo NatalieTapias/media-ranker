a6197c8b3d2124debfabc49194b53521e7ce8d45
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-14 17:21:51 -0700

    Resources routes for Users and Works controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+  
+
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,4 @@
 class WorksController < ApplicationController
+
+  # category,title,creator,publication_year,description
 end
