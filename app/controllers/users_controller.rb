445d42093760e6d8fa39c1eb3fdf4e76b72871c6
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-14 17:28:29 -0700

    Generated User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-  def show
-
-  end
   
-
+  def show
+    user_id = params[:id].to_i
+    @user = User.find_by(id: user)
+    if @user.nil?
+      flash[:error] = "Could not find user with id: #{params[:id]}"
+      redirect_to users_path
+    end
+  end
 end
