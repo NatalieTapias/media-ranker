0c7ab6a6291aadd8b375ab5f89e241ae6fff4c4b
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-15 18:06:22 -0700

    Listing albums, movies, and books by category on index view for HomepagesController

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -3,6 +3,9 @@ class HomepagesController < ApplicationController
   def index
     @works = Work.all
     @top_ten = Work.top_ten
+    @movies = Work.all_movies
+    @albums = Work.all_albums
+    @books = Work.all_books
   end
 
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -3,5 +3,17 @@ class Work < ApplicationRecord
   def self.top_ten
     return Work.order("RANDOM()").limit(10)
   end
+
+  def self.all_albums
+    return Work.where(category: 'album')
+  end
+
+  def self.all_movies
+    return Work.where(category: 'movie')
+  end
+
+  def self.all_books
+    return Work.where(category: 'book')
+  end
   
 end
