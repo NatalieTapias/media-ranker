b562c6effd6fb6684b49605fc4246d3c7ae9bd9f
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-15 18:24:52 -0700

    Updated new works form.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -17,14 +17,14 @@ class WorksController < ApplicationController
   end
   
   def create
-    work = Task.new(work_params)
+    work = Work.new(work_params)
     work.save
     
-    redirect_to tasks_path
+    redirect_to work_path(work)
   end
 
   def edit 
-    work_id = work_params[:id]
+    work_id = params[:id]
     @work = Work.find_by(id: work_id)
 
     if @work.nil?
@@ -33,31 +33,29 @@ class WorksController < ApplicationController
     end
   end
   
-def update 
-  work = Work.find_by(id: work_params[:id])
+  def update 
+    work = Work.find_by(id: params[:id])
 
-  if work.nil?
-    flash[:error] = "Could not find work with id: #{work_params[:id]}"
-      redirect_to root_path
-          return
+    if work.nil?
+      flash[:error] = "Could not find work with id: #{params[:id]}"
+        redirect_to root_path
+            return
+    end
+
+    work.update_attributes(work_params)
+
+    # add some flash success message here
+
+    redirect_to works_path(work)
   end
-  work.title = work_params[:title]
-  work.description = work_params[:description]
-  work.creator = work_params[:creator]
-  work.creator = work_params[:publication_year]
-  work.save
 
-  # add some flash success message here
+  def destroy
+  end
 
-  redirect_to works_path(work)
-end
+  private
 
-def destroy
-end
-
-private
-def work_params
-  return params.require(:work).permit(:title, :description, :id, :creator, :publication_year)
-end
+  def work_params
+    return params.require(:work).permit(:title, :description, :creator, :publication_year, :category)
+  end
 
 end
