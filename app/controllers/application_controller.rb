44f053f85d30bed5d1d8447eb522de42d9639c91
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-17 15:48:48 -0700

    Added private method to return @current_user based on which user is stored in session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
- 
+
+
   private
   def current_user
     if session[:user_id] 
-      return User.find_by(id: session[:user_id])
+    @current_user = User.find_by(id: session[:user_id])
+      return @current_user
     end
   end
 end
