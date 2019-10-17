c70d9b3f83c20d0130915db1962b15d44bc405cc
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-17 11:58:03 -0700

    Added resources for VotesController actions

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,16 @@
 class VotesController < ApplicationController
+
+  def index
+    @votes = Vote.all
+  end
+
+  def show
+    # find votes
+    vote = 
+  end
+
+  def new
+    @vote = Vote.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :works
 resources :users
+resources :votes
 
 get "/login", to: "users#login_form", as: "login"
 post "/login", to: "users#login"
