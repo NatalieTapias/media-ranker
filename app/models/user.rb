7d548a913cb6a96363b8b9d4ebc93cc6c2b4ded8
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-18 15:24:49 -0700

    yml files for users and votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_many :votes
 
   validates :work_id, presence: true
+  validates :user_id, presence: true
   validates :user_id, uniqueness: true
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relations" do
+    it "should have many votes" do
+    end
+  end
+
+  describe "validations" do
+    it "should validate presence of work_id" do
+    end
+
+    it "should validate presence of user_id" do
+    end
+
+    it "should validate uniqueness of user_id" do
+    end
+  end
+
+  describe "custom methods" do
+  end
 end
