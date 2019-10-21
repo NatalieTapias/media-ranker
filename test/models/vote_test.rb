b273750358dfa3d7bc8026a5432a83cefdb0ed20
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-21 06:50:32 -0700

    Vote model test

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,21 @@
 require "test_helper"
 
 describe Vote do
+  let (:vote_on_work) = votes(:vote_on_work)
+
+  let (:vote_second_on_work) = votes(:vote_second_on_work)
+  
   describe "relations" do
-    it "should have many votes" do
+    it "should have one user" do
+      expect(vote_on_work.user_id).must_equal 1
+      expect(vote_second_on_work.user_id).must_equal 1
     end
+
+   it "should have one work" do
+    expect(vote_on_work.work_id).must equal 1
+    expect(vote_second_on_work.work_id).must equal 1
+   end
+
   end
 
   describe "validations" do
