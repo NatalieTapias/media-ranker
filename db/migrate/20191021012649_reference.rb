d5e814fdc35ce8768162be9913f30f89fae79268
Natalie Tapias <nataliemtapias@gmail.com>
2019-10-21 14:50:28 -0700

    Fixing my migrations

--- db/migrate/20191021012649_reference.rb
+++ db/migrate/20191021012649_reference.rb
@@ -1,6 +1,9 @@
 class Reference < ActiveRecord::Migration[5.2]
   def change
+    remove_column :votes, :work_id
+    remove_column :votes, :user_id
     add_reference :votes, :work, foreign_key: :true
     add_reference :votes, :user, foreign_key: :true
+  
   end
 end
